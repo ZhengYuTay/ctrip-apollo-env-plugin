841047de6639567700cf71eaf19374ac4b0de4ea
Kael Zhang <i+github@kael.me>
2019-04-07 20:45:09 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 const apollo = require('ctrip-apollo')
 
+const {AVAILABLE_OPTIONS} = apollo
+
+const createKey = (...args) =>
+  Buffer.from(args.join('|')).toString('base64')
+
+const uniqueKey = options => createKey(
+  ...AVAILABLE_OPTIONS.map(key => options[key])
+)
+
 class ApolloEnvPlugin {
   constructor ({
-
+    envs,
+    ...apolloOptions
   } = {}) {
-
+    this._apolloOptions = apolloOptions
+    this._envs = envs
   }
 
-  apply () {
+  apply (lifecycle) {
+    const apollos = {}
 
+    // post env
+    lifecycle.plugin('env', () => {
+      const tasks = []
+
+      Object.keys(this._envs).forEach(k => {
+        const config = this._envs[k]
+        const {
+          key,
+          ...opts
+        } = typeof config === 'string'
+          ? {
+            key: config
+          }
+          : config
+
+        const options = {
+          ...this._apolloOptions,
+          ...opts
+        }
+
+        const id = uniqueKey(options)
+      })
+    })
   }
 }
