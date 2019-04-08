24925e166a57681266a6f62ab34d8c21449c2d76
Kael Zhang <i+github@kael.me>
2019-04-08 10:35:11 +0800

    WIp

--- src/index.js
+++ src/index.js
@@ -16,32 +16,47 @@ class ApolloEnvPlugin {
   } = {}) {
     this._apolloOptions = apolloOptions
     this._envs = envs
+    this._apollos = Object.create(null)
+    this._tasks = []
+  }
+
+  add (envKey, config) {
+    const {
+      key,
+      ...opts
+    } = typeof config === 'string'
+      ? {
+        key: config
+      }
+      : config
+
+    const options = {
+      ...this._apolloOptions,
+      ...opts
+    }
+
+    this._add(key, options)
+  }
+
+  _add (options) {
+    const id = uniqueKey(options)
+    if (id in this._apollos) {
+      return
+    }
+
+    const apolloClient = apollo(options)
+    const this._apollos[id] = apolloClient
   }
 
   apply (lifecycle) {
-    const apollos = {}
-
     // post env
     lifecycle.plugin('env', () => {
       const tasks = []
 
       Object.keys(this._envs).forEach(k => {
         const config = this._envs[k]
-        const {
-          key,
-          ...opts
-        } = typeof config === 'string'
-          ? {
-            key: config
-          }
-          : config
 
-        const options = {
-          ...this._apolloOptions,
-          ...opts
-        }
 
-        const id = uniqueKey(options)
       })
     })
   }
