2e073a34f2cf3286fecd2a18f007e0f32135e14f
Kael Zhang <i+github@kael.me>
2019-04-08 11:11:07 +0800

    completes main

--- src/index.js
+++ src/index.js
@@ -18,9 +18,65 @@ class ApolloEnvPlugin {
     this._envs = envs
     this._apollos = Object.create(null)
     this._tasks = []
+    this._envKeyConfig = Object.create(null)
   }
 
-  add (envKey, config) {
+  _findEnvKey (key, namespace, cluster) {
+    for (const envKey in this._envKeyConfig) {
+      const config = this._envKeyConfig[envKey]
+      if (
+        config.key === key
+        && config.namespace === config.namespace
+        && config.cluster === config.cluster
+      ) {
+        return envKey
+      }
+    }
+  }
+
+  _applyChange ({
+    newValue, key
+  }, namespace, cluster) {
+    const envKey = this._findEnvKey(key, namespace, cluster)
+    if (!envKey) {
+      return
+    }
+
+    process.env[envKey] = newValue
+  }
+
+  _add (envKey, key, options) {
+    const id = uniqueKey(options)
+
+    const defined = id in this._apollos
+    const client = defined
+      ? this._apollos[id]
+      : this._apollos[id] = apollo(options)
+
+    const {
+      namespace,
+      cluster
+    } = client
+
+    this._envKeyConfig[envKey] = {
+      key,
+      id,
+      namespace,
+      cluster
+    }
+
+    if (defined) {
+      return
+    }
+
+    apolloClient.on('change', e => {
+      this._applyChange(e, namespace, cluster)
+    })
+
+    this._tasks.push(() => apolloClient.ready())
+  }
+
+  _addEnv (envKey, config) {
     const {
       key,
       ...opts
@@ -35,29 +91,28 @@ class ApolloEnvPlugin {
       ...opts
     }
 
-    this._add(key, options)
+    this._add(envKey, key, options)
   }
 
-  _add (options) {
-    const id = uniqueKey(options)
-    if (id in this._apollos) {
-      return
-    }
-
-    const apolloClient = apollo(options)
-    const this._apollos[id] = apolloClient
+  _setEnv () {
+    Object.keys(this._envKeyConfig).forEach(envKey => {
+      const {key, id} = this._envKeyConfig[envKey]
+      const apollo = this._apollos[id]
+      process.env[envKey] = apollo.get(key)
+    })
   }
 
   apply (lifecycle) {
     // post env
-    lifecycle.plugin('env', () => {
+    lifecycle.plugin('env', async () => {
       const tasks = []
 
       Object.keys(this._envs).forEach(k => {
-        const config = this._envs[k]
-
-
+        this._addEnv(k, this._envs[k])
       })
+
+      await Promise.all(this._tasks)
+      this._setEnv()
     })
   }
 }
